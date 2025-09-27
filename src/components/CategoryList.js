b47e6f964aa982e2db49f26a1ff565fb3183a615
MarcosC <marcos_45cerezo@hotmail.com>
2025-09-27 19:35:30 -0300

    guarde en mi rama

--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -42,9 +42,14 @@ class CategoryList extends LitElement {
   // funcion para mostrar la categoria apretada y mandarle a productos-list
   handleClick(cat) {
     console.log("toque la categoria", cat);
-    //para aplicar el 'animate-flash, guardo el id
+    //para aplicar el 'animate-pulse, guardo el id
+     // este if es para cuando deselecionas la categoria, para que no quede la animacion activa 
+  if (this.selectedCategoryId === cat.id) {
+    this.selectedCategoryId = null;
+  } else {
+    // si presionaste otra categoria, le agrega la anmiacion 
     this.selectedCategoryId = cat.id;
-
+  }
     //esta parte es para aplicar filtros, enviandole a la la funcion que esta en ProductList.js
     const productosList = document.querySelector("productos-list");
     if (productosList) {
@@ -65,26 +70,27 @@ class CategoryList extends LitElement {
     // animate-ping
     // animate-bounce
     // animate-pulse
-    // animate-spin
+    // animate-spin                  <h2 class="text-xl font-bold text-gray-700">${cat.title}</h2>
+
     return html`
       <div class="grid grid-cols-2 md:grid-cols-4 gap-4 mb-8">
         ${this.categories.map(
           (cat) => html`
             <div
-              class="p-4 bg-gray-100 rounded-lg shadow text-center transition hover:scale-105 cursor-pointer
+              class="p-0 bg-gray-100 rounded-lg shadow text-center transition hover:scale-105 cursor-pointer
               ${this.selectedCategoryId === cat.id
                 ? "animate-pulse bg-green-50"
                 : ""}"
               @click=${() => this.handleClick(cat)}
             >
               <div>
-                <h2 class="text-xl font-bold text-gray-700">${cat.title}</h2>
                 <img
                   src="http://161.35.104.211:8000${cat.picture}"
                   alt="${cat.title}"
-                  class="w-1/2 object-contain mx-auto mix-blend-multiply brightness-110 mb-2"
+                    class="w-full h-48 object-cover mx-auto mix-blend-multiply brightness-110 mb-0 rounded"
                 />
               </div>
+              <h2 class="text-xl font-bold text-red-700">${cat.title}</h2>
             </div>
           `
         )}
